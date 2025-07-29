fb1e0bf669646994fdd2bc258ead25d8fc6f3dec
AlessaFG <a.grande06192019@gmail.com>
2025-07-29 18:42:48 +0200

    Rimozione file inutili

--- Prova RL/create_env.m
+++
@@ -1,3 +0,0 @@
-obsInfo = rlNumericSpec([12 1], 'LowerLimit', -inf, 'UpperLimit', inf);
-actInfo = rlNumericSpec([12 1], 'LowerLimit', 0, 'UpperLimit', 1);
-env = rlFunctionEnv(obsInfo, actInfo, "quadrotorStepFunction", "myResetFunction");
--- Prova RL/reference_trajectory.m
+++
@@ -1,9 +0,0 @@
-function pos_ref = reference_trajectory(t)
-    if t < 5
-        pos_ref = [2*t/5; 0; 2];
-    elseif t < 10
-        pos_ref = [2; 2*(t-5)/5; 2];
-    else
-        pos_ref = [2 - 2*(t-10)/5; 2; 2];
-    end
-end
